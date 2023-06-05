d4617111d515db5acf2897a649741e7da84d5813
Christian Franzen <13965040+cfranzen@users.noreply.github.com>
2023-06-05 07:56:16 +0200

    Moved cloudrun config to terraform

--- terraform/gcp/service.tf
+++ terraform/gcp/service.tf
@@ -1,3 +1,44 @@
 resource "google_service_account" "service_sa" {
   account_id = "cinema-tickets-service-sa"
 }
+
+resource "google_cloud_run_v2_service" "cinema-tickets-service" {
+  name     = "cinema-tickets-service"
+  project  = local.project
+  location = local.region
+  ingress  = "INGRESS_TRAFFIC_ALL"
+
+  template {
+    scaling {
+      min_instance_count = 0
+      max_instance_count = 1
+    }
+
+    volumes {
+      name = "cloudsql"
+      cloud_sql_instance {
+        instances = [google_sql_database_instance.postgres-db-instance.connection_name]
+      }
+    }
+
+    containers {
+      # We use an empty image here since we refresh that immage using Github action
+      image = "us-docker.pkg.dev/cloudrun/container/hello"
+
+      env {
+        name  = "SPRING_PROFILES_ACTIVE"
+        value = "gcp"
+      }
+      volume_mounts {
+        name       = "cloudsql"
+        mount_path = "/cloudsql"
+      }
+    }
+    service_account = google_service_account.service_sa.email
+  }
+
+  traffic {
+    type    = "TRAFFIC_TARGET_ALLOCATION_TYPE_LATEST"
+    percent = 100
+  }
+}
