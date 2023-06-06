b8536b0831c6a42821074d6ce23e88bb1c7d9079
Ferdinand Ade <ferdinand.ade@otto.de>
2023-06-06 10:37:23 +0200

    todos, added seocnd controller

--- src/main/kotlin/de/codecentric/workshop/hexagonal/cinema/tickets/MovieRecommendationController.kt
+++ src/main/kotlin/de/codecentric/workshop/hexagonal/cinema/tickets/MovieRecommendationController.kt
@@ -6,13 +6,14 @@ import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class MoviePosterController {
+class MovieRecommendationController {
 
     @GetMapping("/movie/{id}/poster")
     fun getMoviePoster(@PathVariable("id") movieId: String): ResponseEntity<String> {
-        // fetch movie, which is the poster ID/ url
-        // fetch poster
-        // return poster
+        // fetch user, with history of his visited movies and rating or favorites
+        // fetch current movies
+        // calculate which movies the user could like
+        // return json
 
         return ResponseEntity.ok().body("Hello world $movieId")
     }
